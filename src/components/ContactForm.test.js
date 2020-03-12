5a62b9b24812f3229ad6e7a4bd825ea171886828
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-12 16:48:00 -0600

    jammed up with the submit button

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,7 +1,30 @@
 import React from 'react';
-import { render, fireEvents } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
 test('renders correctly', ()=> {
     render(<ContactForm/>)
+})
+
+test('form will add fname, lname, email, and message to data in state and display on the page', () => {
+    const { getByLabelText, findByDisplayValue, findAllByText } = render(<ContactForm/>);
+
+    // query for the form inputs
+    const firstNameInput = getByLabelText(/first name/i);
+    const lastNameInput = getByLabelText(/last name/i);
+    const emailInput = getByLabelText(/email/i);
+    const messageInput = getByLabelText(/message/i);
+
+    //fireEvent function from RTL to fill inputs
+    fireEvent.change(firstNameInput, { target: { name: 'firstName', value: 'Mic' } });
+    fireEvent.change(lastNameInput, { target: { name: 'lastName', value: 'Callahan' } });
+    fireEvent.change(emailInput, { target: { name: 'email', value: 'Callahan@gmail.com' } });
+    fireEvent.change(messageInput, { target: { name: 'message', value: 'Here is a message' } });
+
+    // const submitButton = findByDisplayValue(submit);
+
+    // fireEvent.click(submit);
+
+    findAllByText(/callahan/i);
+
 })
